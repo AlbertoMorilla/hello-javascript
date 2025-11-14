ae29396a851ac7e3d7ec14ce146c542f43e8a505
AlbertoMorilla <albertomorillaoficial@gmail.com>
2025-11-14 20:30:51 +0100

    ejercicio casi terminado

---
+++ 09-conditionals-exercise.js
@@ -0,0 +1,38 @@
+// if/else/else if/ternaria
+
+// 1. Imprime por consola tu nombre si una variable toma su valor.
+
+let Name = "Alberto"
+
+if (Name == "Alberto") {
+    console.log("Tu nombre es Alberto")
+} else {
+    console.log("Tu nombre no es Alberto")
+}
+// 2. Imprime por consola un mensaje si el usuario y contraseÃ±a concide con unos establecidos.
+
+let User = "admin"
+let Password = 1234
+
+if (User == "admin" && Password == 1234) {
+    console.log("Acceso permitido")
+} else {
+    console.log("Acceso denegado")
+}
+// 3. Verifica si un nÃºmero es positivo, negativo o cero e imprime un mensaje.
+
+// 4. Verifica si una persona puede votar o no (mayor o igual a 18) e indica cuÃ¡ntos aÃ±os le faltan.
+
+// 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable dependiendo de la edad .
+
+// 6. Muestra en que estaciÃ³n del aÃ±o nos encontramos dependiendo del valor de una variable "mes".
+
+// 7. Muestra el nÃºmero de dÃ­as que tiene un mes dependiendo de la variable del ejercicio anterior.
+
+// switch
+
+// 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma.
+
+// 9. Usa un switch para hacer de nuevo el ejercicio 6.
+
+// 10. Usa un switch para hacer de nuevo el ejercicio 7.
